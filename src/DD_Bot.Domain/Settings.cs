029112d60bd7a94eb3c0e23903fd9a3eb922fbf9
Maxim Kovac <washambala@yahoo.de>
2022-06-28 14:18:16 +0200

    removed unnecessary using directives

--- src/DD_Bot.Domain/Settings.cs
+++ src/DD_Bot.Domain/Settings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DD_Bot.Domain
+﻿namespace DD_Bot.Domain
 {
     public class Settings
     {
